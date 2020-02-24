b693c7b83e248426ee37fe5b129bb5532008bfc5
Ryan Mui <reachryanmui@gmail.com>
2020-02-24 00:30:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,6 +13,7 @@ def create_project_hash
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("span.location-name").text
   # funded: project.css("ul.project-stats li.first.funded strong").text
+  # funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 end
 
