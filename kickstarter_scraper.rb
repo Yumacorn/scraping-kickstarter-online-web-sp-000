e150397f8221d6f3935fc9971dd7bdd107804725
Ryan Mui <reachryanmui@gmail.com>
2020-02-24 00:42:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,6 +15,11 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
+
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    
+  end
   binding.pry
 
 
